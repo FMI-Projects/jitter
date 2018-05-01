825ba23aa3cb88d02af38e8333c1190003f5b7c1
Nimor111 <georgi.bojinov@hotmail.com>
2018-05-01 18:42:04 +0300

    Install some dependencies in client

--- client/src/services/httpService.js
+++ client/src/services/httpService.js
@@ -1,23 +1,22 @@
 export default class HttpService {
-    constructor(requester, baseUrl) {
-        this.requester = requester;
-        this.baseUrl = baseUrl;
-    }
+  constructor(requester, baseUrl) {
+    this.requester = requester;
+    this.baseUrl = baseUrl;
+  }
 
-    get (url) {
-        return this.requester.get(this.baseUrl + url);
-    }
+  get(url) {
+    return this.requester.get(this.baseUrl + url);
+  }
 
-    post(url, data) {
-        return this.requester.post(this.baseUrl + url, data);
-    }
+  post(url, data) {
+    return this.requester.post(this.baseUrl + url, data);
+  }
 
-    put(url, data) {
-        return this.requester.put(this.baseUrl + url, data);
-    }
+  put(url, data) {
+    return this.requester.put(this.baseUrl + url, data);
+  }
 
-    delete(url) {
-        return this.requester.delete(this.baseUrl + url);
-    }
+  delete(url) {
+    return this.requester.delete(this.baseUrl + url);
+  }
 }
-
