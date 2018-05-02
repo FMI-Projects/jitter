533fddfe40d75a1a44dd5a907f164d42ce181b2d
Drago96 <dragproychev@gmail.com>
2018-05-02 11:11:26 +0300

    Fixed minor redux form package naming issue

--- client/src/containers/Register/Register.js
+++ client/src/containers/Register/Register.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class Register extends Component {
+  handleRegister(e) {}
+
   render() {
-    return <div />;
+    return (
+      <form onSubmit={this.handleRegister}>
+        <h1>WORKS</h1>
+      </form>
+    );
   }
 }
 
-export default Register;
+export default reduxForm({ form: "register" })(Register);
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,6 +1,6 @@
-import {createStore, applyMiddleware} from "redux";
-
+import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
+
 import rootReducer from "../store/reducers";
 
 const saga = createSagaMiddleware();
