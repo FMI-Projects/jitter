77c2e48b636794c1ddb6352895cf675d76ef2433
Nimor111 <georgi.bojinov@hotmail.com>
2018-05-03 15:53:49 +0300

    Add register form

--- client/src/components/Register/RegisterForm.js
+++ client/src/components/Register/RegisterForm.js
@@ -1,13 +1,19 @@
-import React, { Fragment } from "react";
+import React, {Fragment} from "react";
 import PropTypes from "prop-types";
-import { Field, reduxForm } from "redux-form";
+import {Field, reduxForm} from "redux-form";
 
 import ValidatedField from "../Form/ValidatedField";
-import {
-  required,
-  email,
-  passwordsMustMatch
-} from "../../utilities/validation";
+import {required, email, passwordsMustMatch} from "../../utilities/validation";
+
+import styles from "./styles";
+
+import {withStyles} from "material-ui/styles";
+import Button from "material-ui/Button";
+import Paper from "material-ui/Paper";
+import Card, {CardHeader} from "material-ui/Card";
+import {Grid} from "material-ui";
+
+import {TextField} from "redux-form-material-ui";
 
 const registerForm = props => {
   let errorMessage;
@@ -17,54 +23,71 @@ const registerForm = props => {
   }
 
   return (
-    <Fragment>
-      <h2> Sign up </h2>
-      {errorMessage}
-      <form onSubmit={props.handleSubmit}>
-        <div>
+    <div className={props.classes.base}>
+      <Grid container>
+        <Card className={props.classes.card}>
+          <CardHeader
+            classes={{title: props.classes.cardTitle}}
+            title="Register"
+          />
+        </Card>
+      </Grid>
+      <Paper className={props.classes.headline}>
+        {errorMessage}
+        <form className={props.classes.form} onSubmit={props.handleSubmit}>
           <div>
-            <Field
-              name="email"
-              component={ValidatedField}
-              type="email"
-              label="Email"
-              validate={[required, email]}
-            />
+            <div>
+              <Field
+                className={props.classes.textField}
+                name="email"
+                component={TextField}
+                type="email"
+                label="Email"
+                validate={[required, email]}
+              />
+            </div>
           </div>
-        </div>
-        <div>
           <div>
-            <Field
-              name="password"
-              component={ValidatedField}
-              type="password"
-              label="Password"
-              validate={[required]}
-            />
+            <div>
+              <Field
+                className={props.classes.textField}
+                name="password"
+                component={TextField}
+                type="password"
+                label="Password"
+                validate={[required]}
+              />
+            </div>
           </div>
-        </div>
-        <div>
           <div>
-            <Field
-              name="confirmPassword"
-              component={ValidatedField}
-              type="password"
-              label="Confirm Password"
-              validate={[passwordsMustMatch]}
-            />
+            <div>
+              <Field
+                className={props.classes.textField}
+                name="confirmPassword"
+                component={TextField}
+                type="password"
+                label="Confirm Password"
+                validate={[passwordsMustMatch]}
+              />
+            </div>
           </div>
-        </div>
-        <div>
-          <button type="submit">Register</button>
-        </div>
-      </form>
-    </Fragment>
+          <Button
+            className={props.classes.button}
+            variant="raised"
+            color="primary"
+            type="submit">
+            Register
+          </Button>
+        </form>
+      </Paper>
+    </div>
   );
 };
 
 registerForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
-  errorMessage: PropTypes.string
+  errorMessage: PropTypes.string,
+  classes: PropTypes.object.isRequired,
 };
 
-export default reduxForm({ form: "register" })(registerForm);
+export default withStyles(styles)(reduxForm({form: "register"})(registerForm));
