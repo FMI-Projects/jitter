d61e9e2208c4a895d59fa52533f46e02a5589f87
Nimor111 <georgi.bojinov@hotmail.com>
2018-05-03 15:24:24 +0300

    Login form done

--- client/src/components/Login/LoginForm.js
+++ client/src/components/Login/LoginForm.js
@@ -1,13 +1,26 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import {Field, reduxForm} from "redux-form";
+import {withStyles} from "material-ui/styles";
 
-import ValidatedField from "../Form/ValidatedField";
+import styles from "./styles";
+
 import {required, email} from "../../utilities/validation";
+
+import Button from "material-ui/Button";
+import Paper from "material-ui/Paper";
+import Card, {CardHeader} from "material-ui/Card";
+import {Grid} from "material-ui";
 
 import {TextField} from "redux-form-material-ui";
 
 class LoginForm extends Component {
+  static propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    errorMessage: PropTypes.string,
+    classes: PropTypes.object.isRequired,
+  };
+
   render() {
     let errorMessage;
 
@@ -15,39 +28,51 @@ class LoginForm extends Component {
       errorMessage = <div className="info-red">{this.props.errorMessage}</div>;
     }
 
+    const {classes} = this.props;
+
     return (
-      <div style={{textAlign: "center"}}>
-        <h2>Sign in</h2>
-        {errorMessage}
-        <form onSubmit={this.props.handleSubmit}>
-          <div>
-            <Field
-              name="email"
-              component={TextField}
-              label="Email"
-              validate={[required, email]}
-              autoFocus={true}
-            />
-          </div>
-          <div>
-            <Field
-              name="password"
-              component={TextField}
-              label="Password"
-              type="password"
-              validate={[required]}
-            />
-          </div>
-          <button type="submit">Sign in</button>
-        </form>
+      <div className={classes.base}>
+        <Grid container>
+          <Card className={classes.card}>
+            <CardHeader classes={{title: classes.cardTitle}} title="Sign in" />
+          </Card>
+        </Grid>
+        <Paper className={classes.headline}>
+          {errorMessage}
+          <form className={classes.form} onSubmit={this.props.handleSubmit}>
+            <div>
+              <Field
+                className={classes.textField}
+                name="email"
+                component={TextField}
+                label="Email"
+                validate={[required, email]}
+                autoFocus={true}
+              />
+            </div>
+            <div>
+              <Field
+                className={classes.textField}
+                name="password"
+                component={TextField}
+                margin="dense"
+                label="Password"
+                type="password"
+                validate={[required]}
+              />
+            </div>
+            <Button
+              className={classes.button}
+              variant="raised"
+              color="primary"
+              type="submit">
+              Sign in
+            </Button>
+          </form>
+        </Paper>
       </div>
     );
   }
 }
 
-LoginForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
-  errorMessage: PropTypes.string,
-};
-
-export default reduxForm({form: "login"})(LoginForm);
+export default withStyles(styles)(reduxForm({form: "login"})(LoginForm));
---
+++ client/src/components/Login/styles.js
@@ -0,0 +1,34 @@
+const styles = theme => ({
+  base: {
+    marginTop: 200,
+    textAlign: "center",
+  },
+  headline: {
+    width: "25%",
+    height: "300px",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  form: {
+    textAlign: "center",
+    padding: "70px 0",
+  },
+  card: {
+    width: "20%",
+    marginBottom: "-45px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    backgroundColor: "#3F51B5",
+  },
+  cardTitle: {
+    color: "#FFFFFF",
+  },
+  button: {
+    marginTop: "20px",
+  },
+  textField: {
+    width: "50%",
+  },
+});
+
+export default styles;
