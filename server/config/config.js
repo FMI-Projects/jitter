2369842d085e86fef51efb65ce84b54b918788a8
Drago96 <dragproychev@gmail.com>
2018-05-08 16:11:55 +0300

    Added aws access keys config

--- server/config/config.js
+++ server/config/config.js
@@ -2,9 +2,14 @@ const environment = process.env.NODE_ENV || "development";
 
 if (environment === "development" || environment === "test") {
   const config = require("./config.json");
+  const keys = require("./keys.json");
   const environmentConfig = config[environment];
 
   Object.keys(environmentConfig).forEach(key => {
     process.env[key] = environmentConfig[key];
+  });
+
+  Object.keys(keys).forEach(key => {
+    process.env[key] = keys[key];
   });
 }
