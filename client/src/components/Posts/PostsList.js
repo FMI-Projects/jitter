dcbfec529d46754e09a24f51e3ffe0c38f06c2b3
Nimor111 <georgi.bojinov@hotmail.com>
2018-05-09 12:37:43 +0300

    Add posts list component

---
+++ client/src/components/Posts/PostsList.js
@@ -0,0 +1,96 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import styles from "./PostsList.styles";
+import classnames from "classnames";
+
+import {withStyles} from "material-ui/styles";
+import Card, {
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions
+} from "material-ui/Card";
+import Collapse from "material-ui/transitions/Collapse";
+import Avatar from "material-ui/Avatar";
+import IconButton from "material-ui/IconButton";
+import Typography from "material-ui/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+class PostsList extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    posts: PropTypes.arrayOf(PropTypes.object),
+    classes: PropTypes.object,
+    user: PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false
+    };
+
+    this.handleExpandClick = this.handleExpandClick.bind(this);
+  }
+
+  handleExpandClick() {
+    this.setState({expanded: !this.state.expanded});
+  }
+
+  render() {
+    const {posts, classes, user} = this.props;
+
+    return (
+      <div>
+        {posts.map(post => {
+          return (
+            <div key={post._id}>
+              <Card className={classes.card}>
+                <CardHeader
+                  avatar={
+                    <Avatar aria-label="Recipe" className={classes.avatar}>
+                      R
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton>
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title={post.title}
+                  subheader={`${user.firstName} ${user.lastName}`}
+                />
+                {post.imageUrl ? (
+                  <CardMedia className={classes.media} src={post.imageUrl} />
+                ) : null}
+                <CardActions className={classes.actions} disableActionSpacing>
+                  <IconButton
+                    className={classnames(classes.expand, {
+                      [classes.expandOpen]: this.state.expanded
+                    })}
+                    onClick={this.handleExpandClick}
+                    aria-expanded={this.state.expanded}
+                    aria-label="Show more">
+                    <ExpandMoreIcon />
+                  </IconButton>
+                </CardActions>
+                <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                  <CardContent>
+                    <Typography paragraph variant="body2">
+                      {post.content}
+                    </Typography>
+                  </CardContent>
+                </Collapse>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(PostsList);
---
+++ client/src/components/Posts/PostsList.styles.js
@@ -0,0 +1,31 @@
+import red from "material-ui/colors/red";
+
+const styles = theme => ({
+  card: {
+    maxWidth: 600,
+    marginLeft: "28%",
+    marginTop: "2%"
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  actions: {
+    display: "flex"
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    }),
+    marginLeft: "auto"
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+});
+
+export default styles;
