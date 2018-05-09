946b5f85fc9c6fc9d573dee16016bab1636e1036
Nimor111 <georgi.bojinov@hotmail.com>
2018-05-09 16:38:58 +0300

    Make post card a separate component

---
+++ client/src/components/Posts/PostCard/PostCard.js
@@ -0,0 +1,89 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import styles from "./PostCard.styles";
+
+import {withStyles} from "material-ui/styles";
+
+import classnames from "classnames";
+import Card, {
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions
+} from "material-ui/Card";
+import Collapse from "material-ui/transitions/Collapse";
+import Avatar from "material-ui/Avatar";
+import IconButton from "material-ui/IconButton";
+import Typography from "material-ui/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+class PostCard extends Component {
+  static propTypes = {
+    post: PropTypes.object,
+    user: PropTypes.object,
+    classes: PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false
+    };
+
+    this.handleExpandClick = this.handleExpandClick.bind(this);
+  }
+
+  handleExpandClick() {
+    this.setState({expanded: !this.state.expanded});
+  }
+
+  render() {
+    const {post, user, classes} = this.props;
+
+    return (
+      <div>
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="Recipe" className={classes.avatar}>
+                R
+              </Avatar>
+            }
+            action={
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={post.title}
+            subheader={`${user.firstName} ${user.lastName}`}
+          />
+          {post.imageUrl ? (
+            <CardMedia className={classes.media} src={post.imageUrl} />
+          ) : null}
+          <CardActions className={classes.actions} disableActionSpacing>
+            <IconButton
+              className={classnames(classes.expand, {
+                [classes.expandOpen]: this.state.expanded
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show more">
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph variant="body2">
+                {post.content}
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(PostCard);
--- client/src/components/Posts/PostsList.js
+++ client/src/components/Posts/PostsList.js
@@ -1,21 +1,7 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import styles from "./PostsList.styles";
-import classnames from "classnames";
 
-import {withStyles} from "material-ui/styles";
-import Card, {
-  CardHeader,
-  CardMedia,
-  CardContent,
-  CardActions
-} from "material-ui/Card";
-import Collapse from "material-ui/transitions/Collapse";
-import Avatar from "material-ui/Avatar";
-import IconButton from "material-ui/IconButton";
-import Typography from "material-ui/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+import PostCard from "./PostCard/PostCard";
 
 class PostsList extends Component {
   static propTypes = {
@@ -26,75 +12,15 @@ class PostsList extends Component {
     user: PropTypes.object
   };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      expanded: false,
-      expandedKey: ""
-    };
-
-    this.handleExpandClick = this.handleExpandClick.bind(this);
-  }
-
-  handleExpandClick(postId) {
-    this.setState({expanded: !this.state.expanded, expandedKey: postId});
-  }
-
   render() {
-    const {posts, classes, user} = this.props;
+    const {posts, user} = this.props;
 
     return (
       <div>
         {posts.map(post => {
           return (
             <div key={post._id}>
-              <Card className={classes.card}>
-                <CardHeader
-                  avatar={
-                    <Avatar aria-label="Recipe" className={classes.avatar}>
-                      R
-                    </Avatar>
-                  }
-                  action={
-                    <IconButton>
-                      <MoreVertIcon />
-                    </IconButton>
-                  }
-                  title={post.title}
-                  subheader={`${user.firstName} ${user.lastName}`}
-                />
-                {post.imageUrl ? (
-                  <CardMedia className={classes.media} src={post.imageUrl} />
-                ) : null}
-                <CardActions className={classes.actions} disableActionSpacing>
-                  <IconButton
-                    className={classnames(classes.expand, {
-                      [classes.expandOpen]:
-                        post._id === this.state.expandedKey &&
-                        this.state.expanded
-                    })}
-                    onClick={() => this.handleExpandClick(post._id)}
-                    aria-expanded={
-                      post._id === this.state.expandedKey && this.state.expanded
-                    }
-                    aria-label="Show more">
-                    <ExpandMoreIcon />
-                  </IconButton>
-                </CardActions>
-                <Collapse
-                  in={
-                    post._id === this.state.expandedKey && this.state.expanded
-                  }
-                  timeout="auto"
-                  unmountOnExit>
-                  <CardContent>
-                    <Typography paragraph variant="body2">
-                      {post.content}
-                    </Typography>
-                  </CardContent>
-                </Collapse>
-              </Card>
+              <PostCard user={user} post={post} />
             </div>
           );
         })}
@@ -103,4 +29,4 @@ class PostsList extends Component {
   }
 }
 
-export default withStyles(styles)(PostsList);
+export default PostsList;
