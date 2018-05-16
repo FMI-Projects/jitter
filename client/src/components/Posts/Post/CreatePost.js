c690095e78bb8c1117fd116f17449223034ccde3
Nimor111 <georgi.bojinov@hotmail.com>
2018-05-16 12:39:40 +0300

    Refactor create post back to functional component

--- client/src/components/Posts/Post/CreatePost.js
+++ client/src/components/Posts/Post/CreatePost.js
@@ -1,31 +1,23 @@
-import React, {Component, Fragment} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {reduxForm} from "redux-form";
 
 import * as actions from "../../../store/actions";
 import PostFormDialog from "./PostFormDialog/PostFormDialog";
 
-class CreatePost extends Component {
-  render() {
-    const {handleSubmit, error, submitting} = this.props;
-    const submit = handleSubmit(actions.postCreate);
+const createPost = props => {
+  const {handleSubmit, error, submitting} = this.props;
+  const submit = handleSubmit(actions.postCreate);
 
-    return (
-      <Fragment>
-        <PostFormDialog
-          submit={submit}
-          error={error}
-          submitting={submitting}
-        />
-      </Fragment>
-    );
-  }
-}
+  return (
+    <PostFormDialog submit={submit} error={error} submitting={submitting} />
+  );
+};
 
-CreatePost.propTypes = {
+createPost.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   error: PropTypes.string,
   submitting: PropTypes.bool.isRequired
 };
 
-export default reduxForm({form: "createPost"})(CreatePost);
+export default reduxForm({form: "createPost"})(createPost);
