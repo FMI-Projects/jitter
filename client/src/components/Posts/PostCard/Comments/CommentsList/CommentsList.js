d476a76a583839a4ba6060e5841bf85ae47db4a8
Nimor111 <georgi.bojinov@hotmail.com>
2018-05-22 00:35:58 +0300

    Change comments list to functional component

--- client/src/components/Posts/PostCard/Comments/CommentsList/CommentsList.js
+++ client/src/components/Posts/PostCard/Comments/CommentsList/CommentsList.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import styles from "./CommentsList.styles";
 import List from "material-ui/List";
@@ -7,30 +7,28 @@ import { withStyles } from "material-ui/styles";
 import CommentForm from "../CommentForm/CommentForm";
 import CommentListItem from "./CommentListItem/CommentListItem";
 
-class CommentsList extends Component {
-  static propTypes = {
-    comments: PropTypes.array.isRequired,
-    classes: PropTypes.object.isRequired,
-    postId: PropTypes.string.isRequired
-  };
+const commentsList = props => {
+  const { comments, classes, postId } = props;
+  return (
+    <div className={classes.root}>
+      <List>
+        {comments.map(comment => {
+          return (
+            <Fragment key={comment._id}>
+              <CommentListItem comment={comment} postId={postId} />
+            </Fragment>
+          );
+        })}
+        <CommentForm postId={postId} formName={`createComment-${postId}`} />
+      </List>
+    </div>
+  );
+};
 
-  render() {
-    const { comments, classes, postId } = this.props;
-    return (
-      <div className={classes.root}>
-        <List>
-          {comments.map(comment => {
-            return (
-              <Fragment key={comment._id}>
-                <CommentListItem comment={comment} postId={postId} />
-              </Fragment>
-            );
-          })}
-          <CommentForm postId={postId} formName={`createComment-${postId}`} />
-        </List>
-      </div>
-    );
-  }
-}
+commentsList.propTypes = {
+  comments: PropTypes.array.isRequired,
+  classes: PropTypes.object.isRequired,
+  postId: PropTypes.string.isRequired
+};
 
-export default withStyles(styles)(CommentsList);
+export default withStyles(styles)(commentsList);
