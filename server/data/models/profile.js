be385c8bdb891ad7196ce81c9acbd94365d3fb18
Nimor111 <georgi.bojinov@hotmail.com>
2018-06-24 14:28:03 +0300

    Add news feed endpoint, fix a few bugs
    * Fix default colour on like -> change it to disabled
    * Add reactions count on post create

--- server/data/models/profile.js
+++ server/data/models/profile.js
@@ -111,11 +111,25 @@ ProfileSchema.statics.getProfileInfo = async function(profileId) {
 ProfileSchema.statics.getFriendIds = async function(profileId) {
   const Profile = this;
 
-  const profileInfo = await Profile.findById(profileId).select("friendships.with");
-
-  const ids = profileInfo.friendships.map(f =>
-    f.with.toHexString()
+  const profileInfo = await Profile.findById(profileId).select(
+    "friendships.with"
   );
+
+  const ids = profileInfo.friendships.map(f => f.with.toHexString());
+
+  return ids;
+};
+
+ProfileSchema.statics.getAcceptedFriendIds = async function(profileId) {
+  const Profile = this;
+
+  const profileInfo = await Profile.findById(profileId).select(
+    "friendships.with friendships.status"
+  );
+
+  const ids = profileInfo.friendships
+    .filter(f => f.status === "Accepted")
+    .map(f => f.with.toHexString());
 
   return ids;
 };
